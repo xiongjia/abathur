6d1166dd57e1e0539360967c6871b914b3ab84f8
xiongjia <lexiongjia@gmail.com>
2016-04-13 23:13:02 +0800

    update grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,13 @@
 'use strict';
 
 module.exports = function (grunt) {
+  var gruntCfg;
+
+  /* load grunt config and plugins */
+  require('load-grunt-tasks')(grunt);
+  gruntCfg = grunt.file.readYAML('grunt_conf.yml');
+  grunt.initConfig(gruntCfg.root);
+
   /* default task */
   grunt.registerTask('default', []);
 };
