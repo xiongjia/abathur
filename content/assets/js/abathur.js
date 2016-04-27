a19efc1a7c358ee9b6668a1290ab4665c4f1c561
xiongjia <lexiongjia@gmail.com>
2016-04-27 22:47:25 +0800

    update js & css

--- content/assets/js/abathur.js
+++ content/assets/js/abathur.js
@@ -5,6 +5,30 @@
     var curPath;
 
     curPath = window.location.pathname;
+    /* install disqus */
+    function installDisqus(commentsElement) {
+      var disqus;
+      disqus = '<div id="ab-disqus">' +
+        '<div id="disqus_thread"></div>' +
+        '<script type="text/javascript">' +
+          'var disqus_shortname = \'myvimwiki\';' +
+          '(function() { ' +
+          '  var dsq = document.createElement(\'script\');' +
+          '  dsq.type = \'text/javascript\';' +
+          '  dsq.async = true;' +
+          '  dsq.src = \'//\' + disqus_shortname + \'.disqus.com/embed.js\';' +
+          '  (document.getElementsByTagName(\'head\')[0] || ' +
+          '   document.getElementsByTagName(\'body\')[0]).appendChild(dsq);' +
+          '})();' +
+        '</script>' +
+        '<noscript>Please enable JavaScript to view the ' +
+        '<a href="http://disqus.com/?ref_noscript">' +
+        'comments powered by Disqus.</a></noscript>' +
+        '<a href="http://disqus.com" class="dsq-brlink" target="_blank">' +
+        'comments powered by <span class="logo-disqus">Disqus</span></a>' +
+        '</div>';
+      commentsElement.prepend(disqus);
+    }
 
     /* adding preamble */
     (function (preamble) {
@@ -49,13 +73,35 @@
         ' src="http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png"></a></p>' +
         '</div></footer>';
       postamble.after($(contents));
+
+      setTimeout(function () { installDisqus(postamble); }, 10);
     })($('#postamble'));
 
     /* right bar */
     (function () {
-      var toc;
+      var toc, isSmallScreen;
+
+      isSmallScreen = $(window).width()  <= 1000;
       toc = $('#table-of-contents');
       toc.wrap('<div class="ab-right">');
+      if (isSmallScreen) {
+        toc.hide();
+        return;
+      }
+      /* google cse */
+      toc.after('<div style="width: 100%;">' +
+                '  <gcse:search></gcse:search>' +
+                '</div><br>');
+      setTimeout(function() {
+        var cx, gcse, s;
+        cx = '008101672356870034238:sapwemkcsbs';
+        gcse = document.createElement('script');
+        gcse.type = 'text/javascript';
+        gcse.async = true;
+        gcse.src = 'https://www.google.com/cse/cse.js?cx=' + cx;
+        s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(gcse, s);
+      }, 10);
     })();
 
     (function () {
