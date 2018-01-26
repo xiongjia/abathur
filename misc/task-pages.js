58d3f3c449f954ba9892667e1b744a1dc4655b4b
xiongjia <lexiongjia@gmail.com>
2018-01-26 10:01:51 +0800

    add js injector method

--- misc/task-pages.js
+++ misc/task-pages.js
@@ -6,13 +6,25 @@ exports = module.exports = (opts) => {
   const path = require('path');
   const { dirs, conf } = opts;
 
-  gulp.task('pages', () => {
+  gulp.task('pages', [ 'sass', 'js' ], () => {
     const posthtml = require('gulp-posthtml');
     const htmlbeautify = require('gulp-html-beautify');
     const htmlmin = require('gulp-htmlmin');
+    const injectStr = require('gulp-inject-string');
+    const inject = require('gulp-inject');
+
     const include = require('posthtml-include')({
       root: path.join(__dirname, '..', dirs.SRC)
     });
+    const injCSSBlock = '<!-- inject:css --><!-- endinject -->';
+    const injJSBlock = '<!-- inject:js --><!-- endinject -->';
+    const injItems = gulp.src([
+      dirs.DIST + '/**/*.css',
+      dirs.DIST + '/**/jquery*.js',
+      dirs.DIST + '/**/bootstrap*.js',
+      dirs.DIST + '/**/bundle*.js'
+    ], { read: false });
+
 
     /* TODO
      *   1. update html head
@@ -23,6 +35,9 @@ exports = module.exports = (opts) => {
         plugins: [ include ],
         options: {}
       })))
+      .pipe(injectStr.after('</title>', injCSSBlock))
+      .pipe(injectStr.before('<div id="postamble"', injJSBlock))
+      .pipe(inject(injItems, { ignorePath: dirs.DIST + '/', relative: false }))
       .pipe(gulpif(conf.DEBUG, htmlbeautify({ indentSize: 2 })))
       .pipe(gulpif(!conf.DEBUG, htmlmin({ collapseWhitespace: true })))
       .pipe(gulp.dest(dirs.DIST));
